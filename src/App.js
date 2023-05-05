d3e708d6643bba8636211411b1c6f1e5fc0259c1
Radhakrishna Kintali <rkintali@intuit.com>
2023-05-05 18:23:32 +0530

    implementd form using redux form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
+import React, { Component } from 'react';
+import SignInForm from './SignInForm';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  handleSignIn = values => {
+    console.log(values);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React x redux-form</h1>
+        </header>
+        <div className="container">
+          <p className="App-intro">
+            Contact Form
+          </p>
+          <SignInForm onSubmit={this.handleSignIn} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
